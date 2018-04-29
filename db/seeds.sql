7a4b3be37e55b264890a13268c0af8a531827f7d
Crystal Hughes <hugachia@me.com>
2018-04-28 23:35:05 -0500

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO burgers (
+burger_name,
+devoured
+) VALUES
+(
+"Turkey Double Cheese Whiz Burger",
+0
+),
+(
+"Beef Jalepeno Mint Burger",
+0
+),
+(
+"Macaroni & Cheese Burger",
+0
+);
