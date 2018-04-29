22ae581bb818bb87e11307b8c8bb610a41bd7b44
Crystal Hughes <hugachia@me.com>
2018-04-28 23:56:29 -0500

    updated server.js file

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-var express = require("express");
-var bodyParser = require("body-parser");
+
 
 var PORT = process.env.PORT || 8080;
 
